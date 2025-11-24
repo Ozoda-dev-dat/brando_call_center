d8af6558aab3183309bf101ce76330594345ee77
Ozoda <aminjonovaozoda596@gmail.com>
2025-11-24 10:25:46 +0500

    Update schema.ts

--- shared/schema.ts
+++ shared/schema.ts
@@ -2,7 +2,6 @@ import { sql } from "drizzle-orm";
 import { pgTable, text, varchar, timestamp } from "drizzle-orm/pg-core";
 import { createInsertSchema } from "drizzle-zod";
 import { z } from "zod";
-
 export const users = pgTable("users", {
   id: varchar("id").primaryKey().default(sql`gen_random_uuid()`),
   username: text("username").notNull().unique(),
